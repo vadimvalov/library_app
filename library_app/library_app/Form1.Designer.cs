a5d9c8dd1324a96f7ab6e49d3b853ee9835148b7
vadimvalov <115623536+vadimvalov@users.noreply.github.com>
2023-04-12 11:07:19 +0600

    upd

--- library_app/library_app/Form1.Designer.cs
+++ library_app/library_app/Form1.Designer.cs
@@ -28,12 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(204, 154);
+            label1.Name = "label1";
+            label1.Size = new Size(201, 37);
+            label1.TabIndex = 0;
+            label1.Text = "Введите логин:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(186, 220);
+            label2.Name = "label2";
+            label2.Size = new Size(219, 37);
+            label2.TabIndex = 1;
+            label2.Text = "Введите пароль:";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(411, 154);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(231, 43);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(411, 220);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(231, 43);
+            textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(478, 340);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 4;
+            button1.Text = "Войти";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(236, 340);
+            button2.Name = "button2";
+            button2.Size = new Size(169, 29);
+            button2.TabIndex = 5;
+            button2.Text = "Зарегистрироваться";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(236, 317);
+            label3.Name = "label3";
+            label3.Size = new Size(101, 20);
+            label3.TabIndex = 6;
+            label3.Text = "Впервые тут?";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Библиотека";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Button button2;
+        private Label label3;
     }
 }
--- library_app/library_app/Form1.cs
+++ library_app/library_app/Form1.cs
@@ -6,5 +6,10 @@ namespace library_app
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
